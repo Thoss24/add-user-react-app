b8fb5938fd64d9842fba1906b3e5c8feb15bbddf
Thoss24 <t.fogarty24@outlook.com>
2023-04-29 17:07:06 +0100

    user component added

---
+++ src/components/user/User.js
@@ -0,0 +1,23 @@
+import styles from './User.module.css'
+
+const User = () => {
+
+    return (
+        <form className={styles.user}>
+            <div className={styles['input-shell']}>
+                <label>Username</label>
+                <input type="text" name='username' id='username'/>
+            </div>
+            <div className={styles['input-shell']}>
+                <label>Age (Years)</label>
+                <input type="text" name='age' id='age'/>
+            </div>
+            <div className={styles['add-user-button-container']}>
+                <button className={styles['add-user-button']} type='submit'>Add User</button>
+            </div>
+
+        </form>
+    )
+}
+
+export default User
