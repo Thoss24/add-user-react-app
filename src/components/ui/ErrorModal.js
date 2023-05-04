c583ff2db7a15404b90d53927239f65f6f15b907
Thoss24 <t.fogarty24@outlook.com>
2023-05-04 19:11:16 +0100

    using portals

--- src/components/ui/ErrorModal.js
+++ src/components/ui/ErrorModal.js
@@ -1,22 +1,47 @@
 import styles from "./ErrorModal.module.css";
-import React, { useState } from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm}/>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles["error-modal-container"]}>
+      <div className={styles.heading}>
+        <h1>{props.head}</h1>
+      </div>
+      <div className={styles["main-content"]}>
+        <p>{props.body}</p>
+        <footer className={styles["button-container"]}>
+          <button className={styles["okay-button"]} onClick={props.onConfirm}>
+            Okay
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.backdrop} />
-      <div className={styles["error-modal-container"]}>
-        <div className={styles.heading}>
-          <h1>{props.head}</h1>
-        </div>
-        <div className={styles["main-content"]}>
-          <p>{props.body}</p>
-          <footer className={styles["button-container"]}>
-            <button className={styles["okay-button"]} onClick={props.onConfirm}>Okay</button>
-          </footer>
-        </div>
-      </div>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      ;
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          head={props.head}
+          body={props.body}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+      ;
+    </Fragment>
   );
 };
 
